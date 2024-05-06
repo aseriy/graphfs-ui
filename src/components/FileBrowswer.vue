d537bbdd12e5c4930c5ec00c5deded6385d03060
Alex Seriy <alex.m.seriy@gmail.com>
2024-05-06 02:25:32 -0400

    Saving work.

--- src/components/FileBrowswer.vue
+++ src/components/FileBrowswer.vue
@@ -13,15 +13,13 @@
     <tr><td colspan="4">Total: {{ fileBrowser.size }}</td></tr>
     <tr>
       <td width="15%">Type</td>
-      <!-- <td>Size</td>
-      <td>Date</td> -->
+      <td>Size</td>
       <td>Name</td>
     </tr>
     <tr v-for="(child, key) in fileBrowser.children" :key='key'>
       <td>{{child.type}}</td>
-      <!-- <td>{{child.size}}</td>
-      <td>{{ child.time }}</td> -->
-      <td @click="updateFileBrowser(fileBrowser.path + '/' + child.basename)">{{child.basename}}</td>
+      <td>{{child.size}}</td>
+      <td align="right" @click="updateFileBrowser(fileBrowser.path + '/' + child.basename)">{{child.basename}}</td>
       <td></td>
     </tr>
   </table>
---
+++ src/components/FileViewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <table width="100%">
+    <tr><td colspan="4">Total: {{ fileBrowser.size }}</td></tr>
+    <tr>
+      <td width="15%">Type</td>
+      <td>Size</td>
+      <td>Name</td>
+    </tr>
+    <tr v-for="(child, key) in fileBrowser.children" :key='key'>
+      <td>{{child.type}}</td>
+      <td>{{child.size}}</td>
+      <td align="right" @click="updateFileBrowser(fileBrowser.path + '/' + child.basename)">{{child.basename}}</td>
+      <td></td>
+    </tr>
+  </table>
+</template>
