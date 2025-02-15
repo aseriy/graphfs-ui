3e88a66c461a18617d699807d0c473e7c6f4d60f
Alex Mykhailov <arizel022@gmail.com>
2025-02-15 15:46:15 -0500

    added sprite.svg

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -15,10 +15,15 @@
             <div v-if="!key">
 
 
-              <svg class="icon icon-home" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 10.5256L10.4993 3.49031C11.382 2.83656 12.618 2.83656 13.5007 3.49031L23 10.5256M4.66667 8.19795V18.6724C4.66667 19.9579 5.76108 21 7.11111 21H16.8889C18.2389 21 19.3333 19.9579 19.3333 18.6724V8.19795" stroke-width="2" stroke-linecap="round"/>
+              <svg class="icon icon-home" width="24" height="24">
+                <use href="images/sprite.svg#icon-home"></use>
               </svg>
 
+
+              <!-- <svg class="icon icon-home" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 10.5256L10.4993 3.49031C11.382 2.83656 12.618 2.83656 13.5007 3.49031L23 10.5256M4.66667 8.19795V18.6724C4.66667 19.9579 5.76108 21 7.11111 21H16.8889C18.2389 21 19.3333 19.9579 19.3333 18.6724V8.19795" stroke-width="2" stroke-linecap="round"/>
+              </svg>
+              -->
 
             </div>
             <div v-else>
